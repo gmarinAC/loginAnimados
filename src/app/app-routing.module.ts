00c949dba03bf4dcd414f814c05a450754f3ebf8
Gustavo <gmarin.sybven@gmail.com>
2023-10-11 13:39:01 -0500

    feature login uno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginOneComponent } from './components/login-one/login-one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'loginOne',
+    component: LoginOneComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
