91b0f61396c8da8110f4bd404c526f0510955c41
Gustavo <gmarin.sybven@gmail.com>
2023-10-15 22:02:18 -0500

    feature: login 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginOneComponent } from './components/login-one/login-one.component';
+import { LoginTwoComponent } from './components/login-two/login-two.component';
 
 const routes: Routes = [
   {
     path:'loginOne',
     component: LoginOneComponent
+  },
+  {
+    path:'loginTwo',
+    component: LoginTwoComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginOneComponent } from './components/login-one/login-one.component';
+import { LoginTwoComponent } from './components/login-two/login-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginOneComponent
+    LoginOneComponent,
+    LoginTwoComponent
   ],
   imports: [
     BrowserModule,
